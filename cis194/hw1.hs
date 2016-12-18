428a48a141f9a13922206635e84696761c2d326e
Steffen Witt <steffen.witt@me.com>
2016-12-18 17:44:01 +0100

    replaced if with guards

--- cis194/hw1.hs
+++ cis194/hw1.hs
@@ -7,29 +7,30 @@ module HomeWork1 where
 -- HW1
 -- Excercise 1
 toDigitsRev :: Integer -> [Integer]
-toDigitsRev a = if a <= 0 
-                then [] 
-                else (a `mod` 10) : toDigitsRev (a `quot` 10)
+toDigitsRev a 
+    |a<= 0      = [] 
+    |otherwise  = (a `mod` 10) : toDigitsRev (a `quot` 10)
 
 toDigits :: Integer -> [Integer]
-toDigits a = if a <= 0
-             then []
-             else toDigits (a `quot` 10) ++  (a `mod` 10) : []
+toDigits a 
+    |a <= 0     = []
+    |otherwise  =  toDigits (a `quot` 10) ++  (a `mod` 10) : []
 
 --Excercise 2
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther xs = if null xs
-                        then []
-                        else if (length (tail xs) `mod` 2) == 0 
-                                then head xs : doubleEveryOther (tail xs)
-                                else ((head xs) * 2) : doubleEveryOther (tail xs)
+doubleEveryOther xs 
+    | null xs   = []
+    | otherwise = if (length (tail xs) `mod` 2) == 0 
+                    then head xs : doubleEveryOther (tail xs)
+                    else ((head xs) * 2) : doubleEveryOther (tail xs)
 
 --Excercise 3
 sumDigits :: [Integer] -> Integer
-sumDigits xs = if null xs then 0 else 
-                                    if (head xs) > 9
-                                        then (head xs) `div` 10 + (head xs) `mod` 10 + sumDigits (tail xs)
-                                        else  (head xs) + sumDigits (tail xs)
+sumDigits xs
+    | null xs   = 0
+    | otherwise = if (head xs) > 9
+                    then (head xs) `div` 10 + (head xs) `mod` 10 + sumDigits (tail xs)
+                    else  (head xs) + sumDigits (tail xs)
 
 --Exercise 4
 validate :: Integer -> Bool
